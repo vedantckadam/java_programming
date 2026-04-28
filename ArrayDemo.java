e6168ed9f6ffb0605f1e66986c768e7fa8734533
Vedant Kadam <vedantkadam2608@gmail.com>
2026-04-28 19:35:41 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+
+public class ArrayDemo {
+    public static void main ( String A[])
+    {
+        int Arr[]={10,20,30,40};
+        
+        int []Brr={10,20,30,40};
+
+        int Crr[]=new int [4];
+
+        Crr[0]=10;
+        Crr[2]=20;
+        Crr[3]=30;
+        Crr[4]=40;
+
+        System.out.println("Elemet in Arr array "+Arr.length);
+        System.out.println("Elemet in Brr array "+Brr.length);
+        System.out.println("Elemet in Crr array "+Crr.length);
+
+        
+
+        
+        
+    }
+    
+}
